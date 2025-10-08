5c8a5efb5704c640fd749f9131b080cfd54dea1d
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2025-10-07 21:00:10 -0500

    CRUD listo para probar

---
+++ blacklist_app/app/services/blacklist_crud.py
@@ -0,0 +1,21 @@
+from app.db import db
+from app.db import Blacklist
+
+class BlacklistCRUD:
+    def __init__(self):
+        self.session = db
+    
+    def addEmailToBlacklist(self, information: dict):
+        try:
+            #Creamos el nuevo registro
+            new_blacklist_email = Blacklist(**information)
+
+            #Hacemos persistencia
+            self.session.add(new_blacklist_email)
+            self.session.commit()
+
+            return new_blacklist_email
+        
+        except Exception as e:
+            self.session.rollback()
+            return None
