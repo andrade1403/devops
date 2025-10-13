67555d464a444d14cc8f276490e8d6090af66f2b
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2025-10-13 11:37:41 -0500

    Corregimos imports quitando blacklist

--- blacklist_app/app/services/blacklist_crud.py
+++ blacklist_app/app/services/blacklist_crud.py
@@ -1,4 +1,4 @@
-from blacklist_app.app.db.blacklist_db import db, Blacklist
+from app.db.blacklist_db import db, Blacklist
 
 class BlacklistCRUD:
     def __init__(self):
--- blacklist_app/application.py
+++ blacklist_app/application.py
@@ -1,7 +1,41 @@
-from blacklist_app import create_app
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
+from app.db.blacklist_db import db
+from flask_jwt_extended import JWTManager
+from app.api.api import BlacklistRegister, BlacklistHealth, BlacklistToken, BlacklistGetEmail, BlacklistDelete
 
-#Creamos app
-application = create_app()
+#Creamos la aplicacion de Flask
+application = Flask(__name__)
+
+#Ponemos configuraciones de la app
+application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://proyectogrupo10:proyectogrupo10@terraform-20251013140729364200000001.cifuwoics1ov.us-east-1.rds.amazonaws.com:5432/proyect_db'
+application.config['JWT_SECRET_KEY'] = 'supersecretkey'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config['PROPAGATE_EXCEPTIONS'] = True
+application.config['JWT_TOKEN_LOCATION'] = ['headers']
+application.config['JWT_HEADER_NAME'] = 'Authorization'
+application.config['JWT_HEADER_TYPE'] = 'Bearer'
+application.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600
+
+#Inicializamos la base de datos
+with application.app_context():
+    db.init_app(application)
+    db.create_all()
+
+#Habilitamos CORS
+CORS(application)
+
+#Inicializamos el JWTManager
+jwt = JWTManager(application)
+
+#Registramos la API RESTful
+api = Api(application)
+api.add_resource(BlacklistRegister, '/v1/blacklists')
+api.add_resource(BlacklistHealth, '/v1/blacklists/health')
+api.add_resource(BlacklistToken, '/v1/blacklists/token')
+api.add_resource(BlacklistGetEmail, '/v1/blacklists/<string:email>')
+api.add_resource(BlacklistDelete, '/v1/blacklists/delete')
 
 if __name__ == '__main__':
     application.run(port = 5000, debug = True)
