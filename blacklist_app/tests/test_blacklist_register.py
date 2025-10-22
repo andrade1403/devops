1ff5e235a19c05e9b2c1af2d8baae8adc6109b71
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2025-10-22 18:28:12 -0500

    Agregamos tests de validaciones email y appId

--- blacklist_app/tests/test_blacklist_register.py
+++ blacklist_app/tests/test_blacklist_register.py
@@ -14,8 +14,8 @@ def app():
 
 def test_blacklist_register_campos_faltantes(app, mocker):
     #Mockeamos la verificacion del JWT para que siempre pase
-    mocker.patch("flask_jwt_extended.view_decorators.verify_jwt_in_request")
-    
+    mocker.patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
+
     #Simulamos un request JSON con solo "email"
     with app.test_request_context(
         '/v1/blacklists',
@@ -32,4 +32,96 @@ def test_blacklist_register_campos_faltantes(app, mocker):
         assert status == 400
         assert response == {
             'msg': 'Hay campos necesarios que no están presentes en la solicitud'
+        }
+
+def test_blacklist_register_email_vacio(app, mocker):
+    #Mockeamos la verificacion del JWT para que siempre pase
+    mocker.patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
+
+    #Simulamos un request JSON con solo "email"
+    with app.test_request_context(
+        '/v1/blacklists',
+        method = 'POST',
+        json = {'email': '', 'appId': '123e4567-e89b-12d3-a456-426614174000'},
+    ):
+        
+        #Importamos la clase BlacklistRegister
+        resource = BlacklistRegister()
+
+        #Llamamos el metodo post a probar la validacion de email vacio
+        response, status = resource.post()
+        
+        #Salidas esperadas
+        assert status == 400
+        assert response == {
+            'msg': 'El campo email no puede estar vacío'
+        }
+
+def test_blacklist_register_appId_vacio(app, mocker):
+    #Mockeamos la verificacion del JWT para que siempre pase
+    mocker.patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
+
+    #Simulamos un request JSON con solo "email"
+    with app.test_request_context(
+        '/v1/blacklists',
+        method = 'POST',
+        json = {'email': 'test@example.com', 'appId': ''},
+    ):
+        
+        #Importamos la clase BlacklistRegister
+        resource = BlacklistRegister()
+
+        #Llamamos el metodo post a probar la validacion de email vacio
+        response, status = resource.post()
+        
+        #Salidas esperadas
+        assert status == 400
+        assert response == {
+            'msg': 'El campo appId no puede estar vacío'
+        }
+
+def test_blacklist_register_invalid_email(app, mocker):
+    #Mockeamos la verificacion del JWT para que siempre pase
+    mocker.patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
+
+    #Simulamos un request JSON con solo "email"
+    with app.test_request_context(
+        '/v1/blacklists',
+        method = 'POST',
+        json = {'email': 'email-invalid', 'appId': '123e4567-e89b-12d3-a456-426614174000'},
+    ):
+        
+        #Importamos la clase BlacklistRegister
+        resource = BlacklistRegister()
+
+        #Llamamos el metodo post a probar la validacion de email vacio
+        response, status = resource.post()
+        
+        #Salidas esperadas
+        assert status == 400
+        assert response == {
+            'msg': 'El email proporcionado no tiene un formato válido'
+        }
+
+def test_blacklist_register_invalid_appId(app, mocker):
+    #Mockeamos la verificacion del JWT para que siempre pase
+    mocker.patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
+
+    #Simulamos un request JSON con solo "email"
+    with app.test_request_context(
+        '/v1/blacklists',
+        method = 'POST',
+        json = {'email': 'test@example.com', 'appId': 'appId-invalid'},
+    ):
+        
+        #Importamos la clase BlacklistRegister
+        resource = BlacklistRegister()
+
+        #Llamamos el metodo post a probar la validacion de email vacio
+        response, status = resource.post()
+        
+        #Salidas esperadas
+        assert status == 400
+        assert response == {
+            'msg': 'El appId proporcionado no es un UUID válido'
         }
